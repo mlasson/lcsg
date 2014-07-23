c2b05b263fd67052a3bf21dddd2a67acaa15abb8
mlasson <marc.lasson@gmail.com>
2014-07-23 14:30:32 +0200

    sweet remote.sh

--- remote.sh
+++ remote.sh
@@ -2,13 +2,36 @@
 message() {
   echo -e "\e[32m$1\e[0m"
 }
-read -p "\e[32mdo you want to send the database (Y/N) ?\e[0m" -n 1 -r
-echo    # (optional) move to a new line
+message "do you want to send the database (Y/N) ?"
+read -n 1 -r
+echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  message "do you want to recompute before (Y/N) ?"
+  read -n 1 -r
+  echo ""
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    message "do you want to regenerate fixture (Y/N) ?"
+    read -n 1 -r
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      cd initdata || exit
+      ./initdata.sh || exit
+    fi
+    message 'resetting ...'
+    cd .. 
+    ./reset.sh
+  fi
+  message 'compression the database ...'
+  zip db.zip lettera/db.sqlite3
   message 'sending the database ...'
-  scp 'lettera/db.sqlite3' vitrine.ovh:srv/lcsg/lettera/ || exit
-  message 'done.'
+  scp db.zip vitrine.ovh:srv/lcsg/ || exit
+  message 'unzipping remotely ...'
+  ssh vitrine.ovh unzip /tmp/db.zip -d ~/srv/lcsg/ || exit
+  message 'erasing local archive'
+  rm -f db.zip 
 fi
 message 'launching remotely'
 ssh vitrine.ovh "srv/lcsg/launch.sh 192.168.1.1:8087"
