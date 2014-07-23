975fd14ec2d9402d823f7b80bb60d8449fc70df2
mlasson <marc.lasson@gmail.com>
2014-07-23 13:39:29 +0200

    scripts again

--- launch.sh
+++ launch.sh
@@ -12,4 +12,4 @@ message 'killing all servers'
 for x in $(ps aux | grep "python manage.py" | grep -v grep | awk '{print $2}'); do kill -s 9 $x; done
 message 'python manage.py runserver'
 screen -d -m python manage.py runserver || exit
-
+message 'finished without error'
--- remote.sh
+++ remote.sh
@@ -2,7 +2,13 @@
 message() {
   echo -e "\e[32m$1\e[0m"
 }
-message 'sending the database'
-scp 'lettera/db.sqlite3' vitrine.ovh:srv/lcsg/lettera/ || exit
+read -p "\e[32mdo you want to send the database (Y/N) ?\e[0m" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  message 'sending the database ...'
+  scp 'lettera/db.sqlite3' vitrine.ovh:srv/lcsg/lettera/ || exit
+  message 'done.'
+fi
 message 'launching remotely'
-scp ssh vitrine.ovh srv/lcsg/launch.sh
+ssh vitrine.ovh "srv/lcsg/launch.sh"
