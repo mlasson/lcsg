7058a9522376ee6bddc9c121a79f8cec400a2ca1
mlasson <marc.lasson@gmail.com>
2014-07-24 15:32:50 +0200

    bug in commands

--- command.sh
+++ command.sh
@@ -10,8 +10,6 @@ source activate.sh || exit
 message "current python version : $(python --version)"
 message 'moving to the root of the project'
 cd lettera  || exit
-message 'killing all servers'
-for x in $(ps aux | grep "python manage.py" | grep -v grep | awk '{print $2}'); do kill -s 9 $x; done
 message "python manage.py runserver $1"
 python manage.py $1 || exit
 message 'finished without error'
