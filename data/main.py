1a42aa8310e026e94c88c043450524c5e6d1e75b
Marc Lasson <marc.lasson@gmail.com>
2018-12-23 17:23:51 +0100

    Mkdir !

--- data/main.py
+++ data/main.py
@@ -1,6 +1,13 @@
 from guichardin import extract
 from dico import generate
+import os
+fixture_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "fixtures")
 
-if __name__ == '__main__':
+def main():
+    if not os.path.exists(fixture_dir):
+        os.mkdir(fixture_dir)
     extract.main()
     generate.main()
+
+if __name__ == '__main__':
+    main()
